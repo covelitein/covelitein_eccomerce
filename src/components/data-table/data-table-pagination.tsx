c160791bd5df386e6e7536144968d79a40b55085
covelitein <abrahamcovenant2004@gmail.com>
2024-12-27 14:02:01 +0100

    pagination adjustments

--- src/components/data-table/data-table-pagination.tsx
+++ src/components/data-table/data-table-pagination.tsx
@@ -22,12 +22,12 @@ export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex flex-col-reverse gap-4 sm:flex-row sm:items-center sm:justify-between px-2">
+    <div className="flex flex-col-reverse sm:gap-4 gap-3 sm:flex-row sm:items-center sm:justify-between px-2 pb-5">
       <div className="flex-1 text-sm text-muted-foreground">
         {table.getFilteredSelectedRowModel().rows.length} of{' '}
         {table.getFilteredRowModel().rows.length} row(s) selected.
       </div>
-      <div className="flex flex-col-reverse gap-4 sm:flex-row sm:items-center sm:gap-6 lg:gap-8">
+      <div className="flex flex-col-reverse sm:gap-4 gap-1 sm:flex-row sm:items-center sm:gap-6 lg:gap-8">
         <div className="flex items-center justify-start gap-2 sm:justify-start">
           <p className="whitespace-nowrap text-sm font-medium">Rows per page</p>
           <Select
@@ -49,7 +49,7 @@ export function DataTablePagination<TData>({
           </Select>
         </div>
         <div className="flex w-full items-center justify-between sm:w-auto sm:justify-end">
-          <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+          <div className="flex sm:w-[100px] items-center justify-center text-sm font-medium">
             Page {table.getState().pagination.pageIndex + 1} of{' '}
             {table.getPageCount()}
           </div>
